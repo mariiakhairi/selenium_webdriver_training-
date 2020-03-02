0e7bccff348a2ffdb3c910187a62aba32f567029
mariiakhairi <mariia_khairi@inbox.ru>
2020-03-02 21:59:49 +0400

    run test on different browsers

--- java-exhample/src/test/java/ru/stqa/training/selenium/TestBase.java
+++ java-exhample/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -4,6 +4,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -14,7 +16,7 @@ public class TestBase {
 
   @Before
   public void start() {
-    driver = new ChromeDriver();
+    driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
