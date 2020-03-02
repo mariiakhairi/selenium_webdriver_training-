8746e49fbe11b57266d0ea68b2ef5d9dc9556160
mariiakhairi <mariia_khairi@inbox.ru>
2020-03-02 22:19:51 +0400

    run test in FF Nightly

--- java-exhample/src/test/java/ru/stqa/training/selenium/TestBase.java
+++ java-exhample/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -4,10 +4,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
@@ -16,7 +19,9 @@ public class TestBase {
 
   @Before
   public void start() {
-    driver = new FirefoxDriver();
+    FirefoxOptions options = new FirefoxOptions();
+    options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Firefox Nightly\\firefox.exe")));
+    driver = new FirefoxDriver(options);
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
