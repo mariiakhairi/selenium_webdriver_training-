df4c3e54611c611cadea39bef5dab228a6e7ee26
mariiakhairi <mariia_khairi@inbox.ru>
2020-03-08 15:18:45 +0400

    click on first menu item

---
+++ java-exhample/src/test/java/ru/stqa/training/selenium/AdminMenuTest.java
@@ -0,0 +1,81 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.concurrent.TimeUnit;
+
+public class AdminMenuTest {
+  private WebDriver driver;
+
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testAdminMenu() throws Exception {
+    gotoTestPage();
+    login();
+    driver.findElement(By.xpath("//li[@id='app-']/a/span[2]")).click();
+    driver.findElement(By.xpath("//td[@id='content']/h1")).click();
+  }
+
+
+  private void login() {
+    driver.findElement(By.name("username")).click();
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).click();
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("login")).click();
+  }
+
+  private void gotoTestPage() {
+    driver.get("http://localhost/litecart/admin/login.php");
+  }
+  public boolean isElementPresent(WebDriver driver, By locator) {
+    try {
+      driver.findElement(locator);
+      return true;
+    } catch (NoSuchElementException ex) {
+      return false;
+    }
+  }
+  public boolean areElementsPresent(WebDriver driver, By locator) {
+    return driver.findElements(locator).size() > 0;
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+
+}
